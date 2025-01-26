bba106c97692fe782a38763a9c487c31ca1a8b65
qdi2k <qdi2k@mail.ru>
2025-01-26 14:43:51 +0300

    Создал модель пользователей

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,7 @@
 import datetime
 
-from sqlalchemy import BigInteger, DateTime, func
+from pydantic import EmailStr
+from sqlalchemy import BigInteger, DateTime, func, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.database import Base
@@ -13,5 +14,23 @@ class ToDo(Base):
     description: Mapped[str]
     completed: Mapped[bool] = mapped_column(default=False)
     created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
+
+
+class User(Base):
+    """Модель пользователя."""
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    email: Mapped[EmailStr] = mapped_column(
+        String(255), unique=True, nullable=False
+    )
+    username: Mapped[str] = mapped_column(
+        String(50), unique=True, nullable=False
+    )
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    data_register: Mapped[datetime.datetime] = mapped_column(
         DateTime, nullable=False, server_default=func.now()
     )
