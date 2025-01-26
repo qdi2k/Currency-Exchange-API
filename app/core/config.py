cfc5a64c9049c351111db2e039934386954d1167
qdi2k <qdi2k@mail.ru>
2025-01-26 16:26:14 +0300

    Изменил тип для времени жизни токена

--- app/core/config.py
+++ app/core/config.py
@@ -36,7 +36,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     @property
     def get_async_database_url(self) -> str:
