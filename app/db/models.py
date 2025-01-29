93dd281917a62b576a97bf438a66240e8b407e04
qdi2k <qdi2k@mail.ru>
2025-01-29 19:03:46 +0300

    Обновил таблицу Users
    Добавил два новых поля для подтверждения email

--- app/db/models.py
+++ app/db/models.py
@@ -31,6 +31,10 @@ class User(Base):
         String(50), nullable=False
     )
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    verified: Mapped[bool] = mapped_column(default=False, nullable=False)
+    verification_token: Mapped[str] = mapped_column(
+        String(255), nullable=True
+    )
     data_register: Mapped[datetime.datetime] = mapped_column(
         DateTime, nullable=False, server_default=func.now()
     )
