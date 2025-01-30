9346226eec7744e322369692aeb67b8a8f488f89
qdi2k <qdi2k@mail.ru>
2025-01-30 04:16:02 +0300

    Добавил новый метод к для обновления одного в репозитории пользователей

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,6 +1,17 @@
+from typing import Dict, Any
+
+from sqlalchemy import update
+
 from app.db.models import User
 from app.repositories.base_repository import Repository
 
 
 class UserRepository(Repository):
     model = User
+
+    async def update_one(self, user_id: int, data: Dict[str, Any]) -> None:
+        """Обновляет одну запись в базе данных по user_id."""
+        stmt = update(self.model).where(
+            self.model.id == user_id
+        ).values(**data)
+        await self.session.execute(stmt)
