ff08c6d35563dde738f2cce1003627d096d8f40f
qdi2k <qdi2k@mail.ru>
2025-02-04 14:58:47 +0300

    возвращаю сообщение об отправленном сообщении на почту + докстринг

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks, Request
+from starlette import status
 
-from app.api.schemas.auth import (ResponseUserLogin, RequestUserCreate,
+from app.api.schemas.user import (ResponseUserLogin, RequestUserCreate,
                                   ResponseUserCreate, RequestUserLogin)
 from app.services.user_service import AuthUserService
 from app.utils.unitofwork import IUnitOfWork, UnitOfWork
@@ -17,12 +18,37 @@ async def get_user_service(
     return AuthUserService(uow)
 
 
-@user_router.post("/register/", response_model=ResponseUserCreate)
+@user_router.post(
+    path="/register/", response_model=ResponseUserCreate,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_user(
-        user_data: RequestUserCreate,
+        request: Request,
+        user_data: RequestUserCreate, background_tasks: BackgroundTasks,
         user_service: AuthUserService = Depends(get_user_service)
 ):
-    return await user_service.registration(user_data=user_data)
+    """
+    ## Регистрация пользователей
+
+    Добавляет запись в БД о пользователе и отправляет на указанную почту
+    письмо для подтверждения регистрации
+    ---
+    #### Принимает на вход следующие параметры:
+    * `password` - пароль пользователя
+
+    * `email` - электронная почта пользователя
+
+    * `username` - имя пользователя
+
+    #### Возвращает следующие параметры:
+    * `message` - сообщение о том, что было отправлено письмо на почту для
+    подтверждения регистрации
+    ---
+    """
+    return await user_service.registration(
+        request=request, user_data=user_data,
+        background_tasks=background_tasks
+    )
 
 
 @user_router.post("/login/", response_model=ResponseUserLogin)
