b19b818e1cc0b7e672c910c5b8eea6811255de37
qdi2k <qdi2k@mail.ru>
2025-02-04 14:25:49 +0300

    Функции генерации и проверки токена для подтверждения email.

--- app/core/security.py
+++ app/core/security.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta, timezone
 from typing import Dict, Any, Optional
 
 import jwt
+from itsdangerous import (URLSafeTimedSerializer, BadSignature,
+                          SignatureExpired)
 from passlib.context import CryptContext
 
 from app.core.config import settings
@@ -21,6 +23,28 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     Проверяет, соответствует ли пароль его хэшу.
     """
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def generate_verification_token(user_id: int) -> str:
+    """
+    Генерация токена для подтверждения email.
+    """
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    return serializer.dumps(user_id)
+
+
+def verify_verification_token(
+        token: str, max_age: int = 3600
+    ) -> Optional[int]:
+    """
+    Проверка токена для подтверждения email.
+    """
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    try:
+        user_id = serializer.loads(token, max_age=max_age)
+        return user_id
+    except BadSignature or SignatureExpired:
+        return None
 
 
 async def create_access_token(data: Dict[str, Any]) -> str:
