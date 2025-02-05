499050ee5131e17202706e582414403f83cf5eeb
qdi2k <qdi2k@mail.ru>
2025-02-05 19:36:04 +0300

    Схемы для CurrencyExchange

--- app/api/schemas/currency.py
+++ app/api/schemas/currency.py
@@ -1,7 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Dict
 
 
 class ResponseCurrencyList(BaseModel):
     success: bool
     currencies: Dict[str, str]
+
+
+class RequestCurrencyExchange(BaseModel):
+    from_currency: str = Field(
+        pattern=r'^[A-Za-z]{3}', min_length=3, max_length=3
+    )
+    to_currency: str = Field(
+        pattern=r'^[A-Za-z]{3}', min_length=3, max_length=3
+    )
+    amount: float = 1
+
+
+class ResponseCurrencyExchange(RequestCurrencyExchange):
+    result: float = 1
