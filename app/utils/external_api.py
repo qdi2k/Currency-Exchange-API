d85dc03274211b26f8a55de15a5d02a2a6223a4d
qdi2k <qdi2k@mail.ru>
2025-02-05 19:45:22 +0300

    Новый метод конвертация валюты (Без обработки ошибок)

--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -6,7 +6,9 @@ from fastapi import HTTPException
 from starlette import status
 from requests.models import Response
 
-from app.api.schemas.currency import ResponseCurrencyList
+from app.api.schemas.currency import (ResponseCurrencyList,
+                                      RequestCurrencyExchange,
+                                      ResponseCurrencyExchange)
 from app.core.config import settings
 
 logger = logging.getLogger()
@@ -15,26 +17,43 @@ logger = logging.getLogger()
 class CurrencyAPI:
     """
     Логика работы со сторонним сервисом по конвертации валюты
+
     https://apilayer.com/marketplace/currency_data-api
     """
+
     logger = logging.getLogger()
 
-    headers = {"apikey": settings.CURRENCY_DATA_API}
-    url_get_list = "https://api.apilayer.com/currency_data/list"
+    HEADERS = {"apikey": settings.CURRENCY_DATA_API}
+    URL_GET_LIST = "https://api.apilayer.com/currency_data/list"
+    PRE_URL_CONVERT = "https://api.apilayer.com/currency_data/convert"
 
     async def get_currency_list(self) -> ResponseCurrencyList:
         """Получить список конвертируемых валют."""
-        response = requests.get(url=self.url_get_list, headers=self.headers)
+        response = requests.get(url=self.URL_GET_LIST, headers=self.HEADERS)
         return ResponseCurrencyList(
             **await self.get_json_data_or_503(response=response)
+        )
+
+    async def convert_currency(
+            self, data: RequestCurrencyExchange
+    ) -> ResponseCurrencyExchange:
+        """Конвертация одной валюты в другую."""
+        url = (f"{self.PRE_URL_CONVERT}?to={data.to_currency}"
+               + f"&from={data.from_currency}&amount={data.amount}")
+        json_data = await self.get_json_data_or_503(
+            response=requests.get(url=url, headers=self.HEADERS)
+        )
+        return ResponseCurrencyExchange(
+            **{**data, "result": json_data.get("result")}
         )
 
     async def get_json_data_or_503(
             self, response: Response
     ) -> Dict[str, Any]:
-        """Возврат тела ответа или райз ошибки сервера."""
+        """Возврат тела ответа или логирование и райз ошибки сервера."""
         json_data = response.json()
         if (response.status_code != status.HTTP_200_OK
+                or json_data.get("success") is False
                 or json_data.get("error")):
             self.logger.error(response.status_code)
             self.logger.error(response.text)
